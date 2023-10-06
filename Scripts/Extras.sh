6835dabf950e630d0c401b0a419aa943c69b409f
飞翔的荷兰人 <ilove11502@gmail.com>
2023-10-06 10:35:58 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 #增加主题
-echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+#echo "CONFIG_PACKAGE_luci-theme-$OWRT_THEME=y" >> .config
+#echo "CONFIG_PACKAGE_luci-app-$OWRT_THEME-config=y" >> .config
+
+echo "CONFIG_PACKAGE_luci-theme-design=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
+
+echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
+echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
 
 #根据源码来修改
 if [[ $OWRT_URL != *"lede"* ]] ; then
