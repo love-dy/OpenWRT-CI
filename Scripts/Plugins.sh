3fe1b63883fa8c4394f05c1093322f69f571fdf5
飞翔的荷兰人 <ilove11502@gmail.com>
2023-10-06 10:32:32 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -15,6 +15,10 @@ git clone --depth=1 --single-branch --branch "dev" https://github.com/vernesong/
 #Pass Wall
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall.git
 git clone --depth=1 --single-branch https://github.com/xiaorouji/openwrt-passwall-packages.git
+#AdGuardHome
+git clone --depth 1 --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git
+#ssr-plus+
+git clone --depth 1 --single-branch https://github.com/fw876/helloworld.git
 
 #预置OpenClash内核和GEO数据
 export CORE_VER=https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version
