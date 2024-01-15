0b0076622ebf0a10e366c725d705ff007125e2eb
飞翔的荷兰人 <ilove11502@gmail.com>
2024-01-15 08:00:32 +0800

    202401150800

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -28,10 +28,11 @@ UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "$([[ $WRT_URL == *"lede"* ]
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "$([[ $WRT_URL == *"lede"* ]] && echo "18.06" || echo "master")"
 
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
-#UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
+# UPDATE_PACKAGE "passwall2" "xiaorouji/openwrt-passwall2" "main"
 UPDATE_PACKAGE "passwall-packages" "xiaorouji/openwrt-passwall-packages" "main"
 UPDATE_PACKAGE "helloworld" "fw876/helloworld" "master"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev"
+UPDATE_PACKAGE "adguardhome" "rufengsuixing/luci-app-adguardhome" "master"
 
 if [[ $WRT_URL == *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "homeproxy" "immortalwrt/homeproxy" "dev"
