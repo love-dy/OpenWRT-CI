7fb240cc0d2b5bd132c3287a66bc4ca9c389c7ad
飞翔的荷兰人 <ilove11502@gmail.com>
2024-01-17 09:37:34 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -31,23 +31,9 @@ fi
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
-#默认design主题，增加argon主题
-echo "CONFIG_PACKAGE_luci-theme-argon" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-argon-config" >> ./.config
-
-if [[ $WRT_URL == *"lede"* ]]; then	
-	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-
-	echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_tuic_client=y" >> ./.config
-
+if [[ $WRT_URL == *"lede"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Hysteria=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_TUIC-Client=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NaiveProxy=y" >> ./.config
-	
+	echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
 elif [[ $WRT_URL == *"immortalwrt"* ]]; then
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
