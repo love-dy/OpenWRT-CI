554135824e970bfdb8a8356038ebb8bc29524f53
love-dy <ilove11502@gmail.com>
2024-08-18 23:53:09 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -26,11 +26,6 @@ UPDATE_PACKAGE "design" "0x676e67/luci-theme-design" "$([[ $WRT_REPO == *"lede"*
 
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
-
-#UPDATE_PACKAGE "small" "kenzok8/small" "master"
-#UPDATE_PACKAGE "openwrt-packages" "kenzok8/openwrt-packages" "master"
-#UPDATE_PACKAGE "alist" "kenzok8/openwrt-packages" "master" "pkg"
-
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "adguardhome" "rufengsuixing/luci-app-adguardhome" "master"
 
