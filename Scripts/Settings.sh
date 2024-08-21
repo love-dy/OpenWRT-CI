ce4bf885755100b7fc858c803c537c899386e945
love-dy <ilove11502@gmail.com>
2024-08-21 23:48:21 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -47,16 +47,11 @@ fi
 
 #添加插件
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 
 #科学插件设置
-echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-		
 echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y" >> ./.config
 
-echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_Hysteria=y" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_NaiveProxy=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config		
