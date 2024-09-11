4b56137bf789cc1e4e6c5e1fd87fb77619133cb8
love-dy <ilove11502@gmail.com>
2024-09-12 07:37:28 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -15,30 +15,20 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 
+
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+
 #echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 #echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
-
-#高通平台锁定512M内存
-if [[ $WRT_TARGET == *"IPQ"* ]]; then
-	echo "CONFIG_IPQ_MEM_PROFILE_1024=n" >> ./.config
-	echo "CONFIG_IPQ_MEM_PROFILE_512=y" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
-	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
-fi
 
 #添加插件
 #echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
 #echo "CONFIG_PACKAGE_luci-app-tailscale=y" >> ./.config
 
 #添加科学插件
-echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-
 echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy=y" >> ./.config
-
-echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config	
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
